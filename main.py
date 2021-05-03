bf087d9984771b955c33cb94b76ecd11729d2b75
silvergge <gaetan.gerard27@gmail.com>
2021-05-03 16:19:54 +0200

    add filter if system in namespace is present & add setting modal feature

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, request, abort, session
+import utils
 
 app = Flask(__name__)
+app.secret_key = "aze"
+
+plurals = ["plans", "planrequests", "states", "providers", "cluster-providers", "module-template", "cluster-module-template", "modules"]
+
+namespaces = ["default", "kube-node-lease", "kube-public", "kube-system", "nsx-system", "pks-system"]
 
 @app.route('/')
 def hello():
-    return "Hello world"
+    return render_template('index.html', plural="plans", namespace=None, namespaces=namespaces)
+
+@app.route('/<plural>')
+@app.route('/<plural>/')
+def plural(plural):
+    if plural not in plurals:
+        abort(404)
+    else:
+        pluralToPrint = utils.pluralsTranslation(plural)
+        return render_template("index.html", plural=plural, pluralToPrint=pluralToPrint, namespace=None, namespaces=namespaces)
+
+@app.route('/<plural>/<namespace>', methods=['GET', 'POST'])
+@app.route('/<plural>/<namespace>/', methods=['GET', 'POST'])
+def pluralNamespaced(plural, namespace):
+    if plural not in plurals or namespace not in namespaces:
+        abort(404)
+    else:
+        if plural.startswith('cluster'):
+            disable="disable"
+        else:
+            disable=None
+        pluralToPrint = utils.pluralsTranslation(plural)
+        return render_template("index.html", plural=plural, pluralToPrint=pluralToPrint, disable=disable, namespace=namespace, namespaces=namespaces)
+
+@app.route('/edit')
+def cluster():
+    return render_template('edit.html')
 
     from flask import Flask
---
+++ utils.py
@@ -0,0 +1,20 @@
+
+def pluralsTranslation(plural):
+        if plural == "plans":
+            return "Plans"
+        elif plural == "planrequests":
+            return "Plan requests"
+        elif plural == "states":
+            return "States"
+        elif plural == "providers":
+            return "Providers"
+        elif plural == "cluster-providers":
+            return "Cluster Providers"
+        elif plural == "module-template":
+            return "Module Template"
+        elif plural == "cluster-module-template":
+            return "Cluster Module Template"
+        elif plural == "modules":
+            return "Modules"
+        else:
+            return 404
